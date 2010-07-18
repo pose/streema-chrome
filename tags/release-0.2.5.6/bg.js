b33d5e2cf582c6d5ef4e506383c559c8a3592f44
albertopose@gmail.com <albertopose@gmail.com@8e0eeb7b-6f5c-3afe-f7db-67b899dffdf7>
2010-07-18 08:37:17 +0000

    Tagging release 0.2.5.6

---
+++ tags/release-0.2.5.6/bg.js
@@ -0,0 +1,135 @@
+// Copyright (c) 2010, Alberto Pose. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//
+//   * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation 
+//   and/or other materials provided with the distribution.
+//   * Neither the name of Streema nor the names of its contributors 
+//   may be used to endorse or promote products derived from this software 
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+/*global streema sprintf*/
+
+var $ = function (s) {
+    return document.getElementById(s); 
+};
+
+(function () {
+    if ( typeof streema === 'undefined' ) {
+        throw new Error('config.js must be loaded first');
+    }
+    streema.player = streema.player || {};
+    var selectedRadio, timeout, popup,
+        eNamed = streema.eventBus.addNamedListener, type;
+
+    streema.player.playTimeout = function () {
+        var state;
+        if ( !$('handle') || (!$('handle').playState && 
+                navigator.appVersion.indexOf("Win") !== -1)) {
+            console.log('Passed timeout check :)! with state: ' +  state);
+
+            streema.eventBus.sendRequest({'method': 'player.playing', 
+                                        'status': 'noCheck'});
+            return;
+        }
+
+        state = $('handle').playState;
+        if ( state  !== 3 ) {
+            console.log('Play timeout, state:' + state );
+        
+            $('player').innerHTML = ''; 
+            streema.eventBus.sendRequest({'method': 'player.error'});
+        } else {
+            console.log('Passed timeout check :)! with state: ' +  state);
+
+            streema.eventBus.sendRequest({'method': 'player.playing', 
+                                        'status': 'ok'});
+        }
+    };
+/*
+    chrome.extension.onRequest.addListener( 
+        function (data,sender,sendResponse) {
+            streema.eventBus.sendRequest(data);
+        }
+    );*/
+
+    eNamed( 'ui.play', function (data) {
+        clearTimeout(streema.player.timeout);
+
+        var radio = data.what, types = {'ram': 'audio/x-pn-realaudio'},
+            defaultType = 'application/x-mplayer2';
+        selectedRadio = JSON.parse(radio);
+        type = defaultType;
+        //console.log(radio)
+       /* <embed type="application/x-vlc-plugin" 
+                    pluginspage="http://www.videolan.org" 
+                    version="VideoLAN.VLCPlugin.2" id="player" 
+                    hidden="true" autoplay="off" 
+                    src="' + radio.streams[0].url +'" />
+        */
+
+        if (navigator.appVersion.indexOf("Win")!==-1) {
+            type = selectedRadio.streams[0].type in types ? 
+                types[selectedRadio.streams[0].type] : defaultType;
+        }
+    });
+
+    eNamed( 'player.streema.error', function (data) {
+
+        if ( popup ) {
+            popup.close();
+        }
+
+        $('player').innerHTML = '';
+        // text="audio/mpeg" is a hack for google chrome to play 
+        // the file right
+        $('player').innerHTML = sprintf(
+            '<embed type="%s" id="handle" src="%s" text="audio/mpeg" />', 
+            type, selectedRadio.streams[0].url);
+
+        if ( $('handle').controls ) {
+            $('handle').controls.play();
+        }
+
+        console.log('Setting timeout');
+        timeout = setTimeout ( 
+            streema.player.playTimeout, 
+            streema.config['playback.timeout']);
+        console.log('setting buffering');
+
+    });
+
+    eNamed('player.streema.stop', function () {
+        clearTimeout(timeout);
+
+         if ( popup ) {
+            popup.close();
+        }
+
+       $('player').innerHTML = '' ;
+    });
+
+/*    eNamed('ui.streemaIcon', function () {
+        chrome.extension.sendRequest({'method': 'player.currentRadio',
+                'id': selectedRadio ? selectedRadio.id : undefined});
+    });*/
+
+    console.log('background module loaded ok');
+
+}());
---
+++ tags/release-0.2.5.6/config.js
@@ -0,0 +1,95 @@
+// Copyright (c) 2010, Alberto Pose. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//
+//   * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation 
+//   and/or other materials provided with the distribution.
+//   * Neither the name of Streema nor the names of its contributors 
+//   may be used to endorse or promote products derived from this software 
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+/*global localStorage chrome*/
+
+var streema;
+
+(function () {
+    var getDefaults = function () {
+        var config = {};
+
+        config['analytics.enabled'] = true;
+        config['analytics.account'] = 'UA-16445553-1';
+
+        config['playback.timeout'] = 15000;
+        
+        /* Notifications*/
+        config['notifications.timeout'] = 10000;
+        config['notifications.enabled'] = true;
+        
+        /* Songinfo */
+        config['songinfo.enabled'] = true;
+        config['songinfo.check_every'] = 0.75;
+
+        return config;
+    },
+
+    /* If updating from a previous version, create the key with the default 
+    value if it does not exist */
+    checkNewKeys = function () {
+        var key;
+        var defaults = getDefaults();
+        for ( key in defaults ) {
+            if ( streema.config[key] === undefined ) {
+                streema.config[key] = defaults[key];
+                console.log('Creating key: ' + key + ' with value: ' + 
+                    defaults[key]);
+            }
+        }
+    };
+
+    if ( typeof streema === 'undefined') {
+        streema = {};
+    }
+
+    if ( !streema.config ) {
+        streema.config = {};
+    }
+    
+    streema.saveConfig = function () {
+        localStorage['config'] = JSON.stringify(streema.config);
+        chrome.extension.sendRequest({'method': 'config.refresh'});
+    };
+
+    streema.loadConfig = function () {
+        streema.config = JSON.parse(localStorage['config']);
+        if (navigator.appVersion.indexOf("Mac")!=-1) {
+            config['analytics.enabled'] = false;
+        }
+    };
+
+    if ( !localStorage['config'] ) {
+        streema.config = getDefaults(); 
+        localStorage['config'] = JSON.stringify(streema.config);
+    } else {
+        streema.loadConfig();
+        checkNewKeys();
+    }
+
+    console.log('config module loaded ok');
+}());
+
