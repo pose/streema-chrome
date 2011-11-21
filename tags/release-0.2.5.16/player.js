d2c27c8f45ff93a9f08a47147da679e9afaad2e9
albertopose@gmail.com <albertopose@gmail.com@8e0eeb7b-6f5c-3afe-f7db-67b899dffdf7>
2011-11-21 02:01:34 +0000

    Tagging release 0.2.5.16

---
+++ tags/release-0.2.5.16/player.js
@@ -0,0 +1,108 @@
+// Copyright (c) 2010, Alberto Pose. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//
+//   * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation 
+//   and/or other materials provided with the distribution.
+//   * Neither the name of Streema nor the names of its contributors 
+//   may be used to endorse or promote products derived from this software 
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+/*global window streema ST chrome*/
+
+window.addEventListener('load', function () {
+
+    var eNamed = streema.eventBus.addNamedListener, selectedRadio,
+    player,
+    failed;
+
+    if (typeof ST !== 'undefined') {
+        player = new ST.player.Player(document.getElementById('player'))
+        ST.player.flash.SWF_PATH = 'http://streema.com/static/flash/player.swf';
+        ST.player.flash.PLAYLIST_PROXY_URL_TPL="http://streema.com/proxy/playlist/{sid}";
+    } else {
+        player = {
+        open: function () {
+            return {playable: false};
+        },
+        stop: function () { }
+        };
+    }
+
+    eNamed( 'ui.play', function (data) {
+        if (failed) {
+            streema.eventBus.sendRequest({'method': 'player.streema.stop'});
+        }
+        
+        var radio = data.what, result, st;
+
+        failed = false;
+
+        selectedRadio = JSON.parse(radio);
+
+//        console.log('Sending to streema player: ' + JSON.stringify(selectedRadio));
+        result = player.open(selectedRadio);
+        
+        if ( result.playable ) {
+            player.play();
+            st = player.getState();
+        }
+        console.log(selectedRadio.name + ' is playable by Streema? ' + (result.playable ? 'yes': 'no :('));
+
+
+        if ( !result.playable || (st !== ST.player.api.states.STATE_CONNECTING &&
+            st !== ST.player.api.states.STATE_BUFFERING &&
+            st !== ST.player.api.states.STATE_PLAYING) ) {
+            console.log('Can\'t play radio ' + selectedRadio.name + 
+                ' with streema player.');
+            failed = true;
+            player.stop();
+            streema.eventBus.sendRequest({'method': 'player.streema.error'});
+            return;
+        }
+       
+        setTimeout( function () {
+            streema.eventBus.sendRequest({'method': 'player.playing',
+                'status': 'ok'});
+            }, data.timeout);
+
+    });
+
+    
+    eNamed('ui.stop', function () {
+        player.stop();
+        if ( failed ) {
+            streema.eventBus.sendRequest({'method': 'player.streema.stop'});
+        }
+    });
+
+    eNamed('ui.streemaIcon', function () {
+        chrome.extension.sendRequest({'method': 'player.currentRadio',
+                'id': selectedRadio ? selectedRadio.id : undefined});
+    });
+
+    chrome.extension.onRequest.addListener( 
+        function (data,sender,sendResponse) {
+            streema.eventBus.sendRequest(data);
+        }
+    );
+
+    console.log('player adapter loaded ok');
+
+}, true);
---
+++ tags/release-0.2.5.16/songinfo.js
@@ -0,0 +1,122 @@
+// Copyright (c) 2010, Alberto Pose. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//
+//   * Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation 
+//   and/or other materials provided with the distribution.
+//   * Neither the name of Streema nor the names of its contributors 
+//   may be used to endorse or promote products derived from this software 
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+/*global streema sprintf*/
+
+(function () {
+
+
+    var MINUTES_BETWEEN_SONG_INFO = streema.config['songinfo.check_every'],
+    eNamed = streema.eventBus.addNamedListener, songInfo, radio, timer,
+    updateSongInfo = function () {
+        
+        if ( !streema.config['songinfo.enabled'] ) {
+            return;
+        }
+
+        if ( !radio ) {
+            return;
+        }
+        var newSongInfo, xhr = new XMLHttpRequest();
+        xhr.onreadystatechange =  function() {
+            if ( xhr.readyState === 4 ) {
+                console.log(xhr.responseText);
+                newSongInfo = JSON.parse(xhr.responseText);
+
+                /* New radio does not provide song info, so we need to
+                hide the previous song name */
+                if ( newSongInfo === undefined) {
+                    songInfo = undefined;
+                    return;
+                }
+
+                /* Also, as this radio does not support song info suspend
+                the song name polling */
+                if ( newSongInfo.data === undefined  ||
+                    newSongInfo.data.song === undefined) {
+                    clearInterval(timer);
+                    return;
+                }
+
+                /* Has the songInfo changed? */
+                if (  !songInfo ||
+                    JSON.stringify(songInfo.data) !== 
+                    JSON.stringify(newSongInfo.data)  ) {
+
+                    songInfo = newSongInfo;
+                    streema.eventBus.sendRequest(
+                        {'method': 'song.update', 
+                        'info': JSON.stringify(songInfo.data)}
+                    );
+                    console.log(sprintf('Playing %s by %s', 
+                            songInfo.data.song, 
+                            songInfo.data.artist) );
+                }
+            }
+        };
+
+        xhr.open('GET',sprintf(
+            'http://streema.com/radios/metadata/%d?noCache=1277389681591', 
+            radio.id), 
+            true);
+        xhr.send();
+    };
+
+    eNamed('player.playing', updateSongInfo);
+
+    eNamed('song.checkSong', updateSongInfo);
+
+    eNamed('ui.play', function (data) {
+        if ( timer !== undefined ) {
+            clearInterval(timer);
+        }
+        if ( !streema.config['songinfo.enabled'] ) {
+            return;
+        }
+        radio = JSON.parse(data.what);
+        songInfo = undefined;
+        timer = setInterval(function () { 
+            streema.eventBus.sendRequest({'method': 'song.checkSong'});
+        } , 1000 * 60 * MINUTES_BETWEEN_SONG_INFO);
+    });
+
+    eNamed('ui.stop', function() {
+        if (timer !== undefined) {
+            clearInterval(timer);
+        }
+    });
+    
+    eNamed('player.maybe', function() {
+        if (timer !== undefined) {
+            clearInterval(timer);
+        }
+    });
+
+    updateSongInfo();
+
+    console.log('songinfo module loaded ok');
+
+}());
